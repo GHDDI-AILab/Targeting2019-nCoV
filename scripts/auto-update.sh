11732d4f81c5f85139a45d61bea1a74b5ff4f7e6
Qi Liu <qi.liu@ghddi.org>
2022-09-16 11:10:22 +0800

    git-auto

--- scripts/auto-update.sh
+++ scripts/auto-update.sh
@@ -2,26 +2,28 @@
 
 ROOT="/home/ghddiai/Targeting2019-nCoV"
 cd $ROOT
-echo `pwd`
+echo Current workspace: `pwd`
 
 
 ########################
 # Update markdwon
 ########################
 
-# embed("preclinical_backup.md", "list.html", '<iframe src="list.html">', "preclinical.md")
-# embed("research_progress_backup.md", "ldatopics.html", '<iframe src="ldatopics.html">', "research_progress.md")
-# embed("CN_preclinical_backup.md", "list.html", '<iframe src="list.html">', "CN_preclinical.md")
-# embed("CN_research_progress_backup.md", "ldatopics.html", '<iframe src="ldatopics.html">', "CN_research_progress.md")
+embedding-htmls() {
+    if ! [[ $(git status) =~ "working tree clean" ]]; then
+        echo "embedding-htmls"
 
-python scripts/embed_html.py --source_file "docs/preclinical_backup.md" --embedded_file "docs/file/list.html" --placeholder '<iframe src="list.html">' --target_file "docs/preclinical.md"
+        python scripts/embed_html.py --source_file "docs/preclinical_backup.md" --embedded_file "docs/file/list.html" --placeholder '<iframe src="list.html">' --target_file "docs/preclinical.md"
 
-python scripts/embed_html.py --source_file "docs/research_progress_backup.md" --embedded_file "docs/file/ldatopics.html" --placeholder '<iframe src="ldatopics.html">' --target_file "docs/research_progress.md"
+        python scripts/embed_html.py --source_file "docs/research_progress_backup.md" --embedded_file "docs/file/ldatopics.html" --placeholder '<iframe src="ldatopics.html">' --target_file "docs/research_progress.md"
 
-python scripts/embed_html.py --source_file "docs/CN_preclinical_backup.md" --embedded_file "docs/file/list.html" --placeholder '<iframe src="list.html">' --target_file "docs/CN_preclinical.md"
+        python scripts/embed_html.py --source_file "docs/CN_preclinical_backup.md" --embedded_file "docs/file/list.html" --placeholder '<iframe src="list.html">' --target_file "docs/CN_preclinical.md"
 
-python scripts/embed_html.py --source_file "docs/CN_research_progress_backup.md" --embedded_file "docs/file/ldatopics.html" --placeholder '<iframe src="ldatopics.html">' --target_file "docs/CN_research_progress.md"
+        python scripts/embed_html.py --source_file "docs/CN_research_progress_backup.md" --embedded_file "docs/file/ldatopics.html" --placeholder '<iframe src="ldatopics.html">' --target_file "docs/CN_research_progress.md"
+    fi
+}
 
+embedding-htmls
 
 ########################
 # git auto
